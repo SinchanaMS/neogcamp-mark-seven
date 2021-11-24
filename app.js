d7a612b853567ba60c00592982274dbcbb298e2f
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2021-11-25 02:33:41 +0530

    styling

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnDothraki = document.querySelector("#btn-dothraki")
 var txtInput=document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
 
 function translatedURL(text){
     return serverURL + "?" + "text=" + text
